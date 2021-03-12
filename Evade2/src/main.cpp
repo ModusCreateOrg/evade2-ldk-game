dd9c4dc3073b4475bfe7b8d8e15499e47623ae01
Jay Garcia <jesusdgarcia@gmail.com>
2021-03-12 15:31:49 -0500

    Added arg checking

--- Evade2/src/main.cpp
+++ Evade2/src/main.cpp
@@ -27,6 +27,11 @@ extern "C" void app_main(char *fileName) {
 
 
 int main(int argc, char *argv[]) {
-  app_main(argv[1]);
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+  if (argc < 2) {
+    printf("Usage: ./%s <path.to.ini.file>\n", argv[0]);
+    return 1;
+  }
+#endif  app_main(argv[1]);
   return 0;
 }
