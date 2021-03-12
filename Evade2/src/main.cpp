a5639e7a1947b572bec9f5ad01dfdab675cc36be
Jay Garcia <jesusdgarcia@gmail.com>
2021-03-12 15:21:16 -0500

    Added file arg for ini config file

--- Evade2/src/main.cpp
+++ Evade2/src/main.cpp
@@ -4,20 +4,28 @@ BViewPort   *gViewPort;
 BGameEngine *gGameEngine;
 
 GGame *gGame;
-//TOptions *gOptions;
-
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+#include "NetworkDisplay/NetworkDisplay.h"
+#endif
 /** @file
  * \brief Main entry point for game engine.
  *
  * Initializes the engine and runs the main event loop.
  */
-extern "C" void app_main() {
+// app_main
+extern "C" void app_main(char *fileName) {
   gGame = new GGame();
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+  NetworkDisplay *display = (NetworkDisplay*)&gDisplay;
+//    const char *fileName = "/home/jgarcia/modite-rgb.ini";
+#endif
   gGame->Run();
+  delete gGame;
 }
 
-int main() {
-  app_main();
+
+
+int main(int argc, char *argv[]) {
+  app_main(argv[0]);
   return 0;
 }
-
